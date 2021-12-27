f20f0fcbcc3e0210618d836caac423f4f9c7716f
Ilya Moiseev <ilya.moiseev95@gmail.com>
2021-12-27 16:22:57 +0300

    update README.md

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import get_object_or_404
 from posts.models import Group, Post
 from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .permissions import IsAuthorOrReadOnlyPermission
-from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
-                          PostSerializer)
+from .serializers import (
+    CommentSerializer,
+    FollowSerializer,
+    GroupSerializer,
+    PostSerializer,
+)
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -21,16 +26,12 @@ class PostViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [
-        IsAuthorOrReadOnlyPermission
-    ]
+    permission_classes = [IsAuthorOrReadOnlyPermission]
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs['post_id'])
@@ -49,7 +50,7 @@ class FollowViewSet(
 ):
     serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticated]
-    filter_backends = (filters.SearchFilter,)
+    filter_backends = [DjangoFilterBackend]
     search_fields = ['following__username']
 
     def get_queryset(self):
